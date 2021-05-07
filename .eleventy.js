43478f1e37db1125eb934b6a0bb61e33a72834e6
Amrita <amrita.chanda@gmail.com>
2021-05-07 23:02:24 +0530

    Debug: backdrop-filter blur on safari

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/utils");
+  // eleventyConfig.addPassthroughCopy("./src/fonts");
 
   eleventyConfig.addShortcode("youtube", (id) => {
     return `<div class="video-wrapper"><iframe src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>`;
