4a0b8f86a84591ea475b6ed4936b55806bbdfce4
Amrita <amrita.chanda@gmail.com>
2021-08-09 23:41:49 +0530

    Debug: NG Social section working on chrome; FB page not loading on firefox

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function (eleventyConfig) {
   // eleventyConfig.addPassthroughCopy("./src/fonts");
 
   eleventyConfig.addShortcode("youtube", (id) => {
-    return `<div class="video-wrapper"><iframe src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>`;
+    return `<div class="video-container"><iframe class="responsive-iframe" src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe></div>`;
   });
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
