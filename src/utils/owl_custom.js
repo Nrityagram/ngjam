b08e7893ed8c3fb6a8c198a1ebb4b2a178c10a06
Amrita <amrita.chanda@gmail.com>
2022-03-03 22:28:57 +0530

    WIP: dance prof page with image slider

--- src/utils/owl_custom.js
+++ src/utils/owl_custom.js
@@ -5,30 +5,72 @@ $(document).ready(function () {
     nav: true,
     center: true,
     dots: false,
+    // navText: [
+    //   '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+    //   '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+    // ],
     navText: [
-      '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-      '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+      '<img src="https://res.cloudinary.com/nrityagram/image/upload/v1646318418/chevron-left-2_bta3sl.png">',
+      '<img src="https://res.cloudinary.com/nrityagram/image/upload/v1646318418/chevron-right-2_dped9m.png">',
     ],
     responsive: {
       0: {
         items: 1,
         stagePadding: 0,
       },
+      768: {
+        items: 1,
+        stagePadding: 127,
+      },
       800: {
         items: 1,
-        stagePadding: 150,
+        stagePadding: 132,
+      },
+      1024: {
+        items: 1,
+        stagePadding: 169,
       },
       1200: {
         items: 1,
-        stagePadding: 200,
+        stagePadding: 198,
+      },
+      1300: {
+        items: 1,
+        stagePadding: 213,
       },
       1400: {
         items: 1,
-        stagePadding: 250,
+        stagePadding: 231,
+      },
+      1500: {
+        items: 1,
+        stagePadding: 248,
+      },
+      1600: {
+        items: 1,
+        stagePadding: 264,
       },
       1700: {
         items: 1,
-        stagePadding: 300,
+        stagePadding: 281,
+      },
+      1800: {
+        items: 1,
+        stagePadding: 297,
+      },
+      1920: {
+        items: 1,
+        // 16.5vw is the padding on both sides of center item
+        // 0.165 * 1920 = 317
+        stagePadding: 317,
+      },
+      2000: {
+        items: 1,
+        stagePadding: 330,
+      },
+      2100: {
+        items: 1,
+        stagePadding: 347,
       },
     },
   });
